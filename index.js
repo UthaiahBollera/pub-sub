d4ba1cebcb6feb9ecb6a6437ed5468ec98978a2a
Sanju Uthaiah Bollera <bollera.sanju@outlook.com>
2018-08-06 16:40:38 +0530

    check

--- index.js
+++ index.js
@@ -1,108 +1,117 @@
-/******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
-/******/
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId]) {
-/******/ 			return installedModules[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
-/******/ 			exports: {}
-/******/ 		};
-/******/
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/
-/******/
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
-/******/
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
-/******/
-/******/ 	// define getter function for harmony exports
-/******/ 	__webpack_require__.d = function(exports, name, getter) {
-/******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, {
-/******/ 				configurable: false,
-/******/ 				enumerable: true,
-/******/ 				get: getter
-/******/ 			});
-/******/ 		}
-/******/ 	};
-/******/
-/******/ 	// getDefaultExport function for compatibility with non-harmony modules
-/******/ 	__webpack_require__.n = function(module) {
-/******/ 		var getter = module && module.__esModule ?
-/******/ 			function getDefault() { return module['default']; } :
-/******/ 			function getModuleExports() { return module; };
-/******/ 		__webpack_require__.d(getter, 'a', getter);
-/******/ 		return getter;
-/******/ 	};
-/******/
-/******/ 	// Object.prototype.hasOwnProperty.call
-/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-/******/
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
-/******/
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
-/******/ })
-/************************************************************************/
-/******/ ([
-/* 0 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-class Event {
-  constructor(props) {
-    var events = {};
-    this.subscribe = function(eventKey, callback) {
-      if (events[eventKey] === undefined) {
-        events[eventKey] = [];
-      }
-      if (typeof callback !== "function") {
-        return false;
-      }
-      events[eventKey].push(callback);
-    };
-    this.dispatch = function(eventKey, data) {
-      data = data || null;
-      if (eventKey === undefined) {
-        return false;
-      }
-      events[eventKey].forEach(eCallback => {
-        eCallback(data);
+/******/ (function(modules) {
+  // webpackBootstrap
+  /******/ // The module cache
+  /******/ var installedModules = {}; // The require function
+  /******/
+  /******/ /******/ function __webpack_require__(moduleId) {
+    /******/
+    /******/ // Check if module is in cache
+    /******/ if (installedModules[moduleId]) {
+      /******/ return installedModules[moduleId].exports;
+      /******/
+    } // Create a new module (and put it into the cache)
+    /******/ /******/ var module = (installedModules[moduleId] = {
+      /******/ i: moduleId,
+      /******/ l: false,
+      /******/ exports: {}
+      /******/
+    }); // Execute the module function
+    /******/
+    /******/ /******/ modules[moduleId].call(
+      module.exports,
+      module,
+      module.exports,
+      __webpack_require__
+    ); // Flag the module as loaded
+    /******/
+    /******/ /******/ module.l = true; // Return the exports of the module
+    /******/
+    /******/ /******/ return module.exports;
+    /******/
+  } // expose the modules object (__webpack_modules__)
+  /******/
+  /******/
+  /******/ /******/ __webpack_require__.m = modules; // expose the module cache
+  /******/
+  /******/ /******/ __webpack_require__.c = installedModules; // define getter function for harmony exports
+  /******/
+  /******/ /******/ __webpack_require__.d = function(exports, name, getter) {
+    /******/ if (!__webpack_require__.o(exports, name)) {
+      /******/ Object.defineProperty(exports, name, {
+        /******/ configurable: false,
+        /******/ enumerable: true,
+        /******/ get: getter
+        /******/
       });
-    };
-    this.remove = function(eventkey) {
-      if (eventkey !== undefined) {
-        events[eventkey] = [];
+      /******/
+    }
+    /******/
+  }; // getDefaultExport function for compatibility with non-harmony modules
+  /******/
+  /******/ /******/ __webpack_require__.n = function(module) {
+    /******/ var getter =
+      module && module.__esModule
+        ? /******/ function getDefault() {
+            return module["default"];
+          }
+        : /******/ function getModuleExports() {
+            return module;
+          };
+    /******/ __webpack_require__.d(getter, "a", getter);
+    /******/ return getter;
+    /******/
+  }; // Object.prototype.hasOwnProperty.call
+  /******/
+  /******/ /******/ __webpack_require__.o = function(object, property) {
+    return Object.prototype.hasOwnProperty.call(object, property);
+  }; // __webpack_public_path__
+  /******/
+  /******/ /******/ __webpack_require__.p = ""; // Load entry module and return exports
+  /******/
+  /******/ /******/ return __webpack_require__((__webpack_require__.s = 0));
+  /******/
+})(
+  /************************************************************************/
+  /******/ [
+    /* 0 */
+    /***/ function(module, __webpack_exports__, __webpack_require__) {
+      "use strict";
+      Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+      class Event {
+        constructor(props) {
+          var events = {};
+          this.subscribe = function(eventKey, callback) {
+            if (events[eventKey] === undefined) {
+              events[eventKey] = [];
+            }
+            if (typeof callback !== "function") {
+              return false;
+            }
+            events[eventKey].push(callback);
+          };
+          this.dispatch = function(eventKey, data) {
+            data = data || null;
+            if (eventKey === undefined) {
+              return false;
+            }
+            events[eventKey].forEach(eCallback => {
+              eCallback(data);
+            });
+          };
+          this.remove = function(eventkey) {
+            if (eventkey !== undefined) {
+              events[eventkey] = [];
+            }
+          };
+          this.removeAll = function() {
+            events = [];
+          };
+        }
       }
-    };
-    this.removeAll = function() {
-      events = [];
-    };
-  }
-}
-/* harmony export (immutable) */ __webpack_exports__["default"] = Event;
+      /* harmony export (immutable) */ __webpack_exports__["default"] = Event;
 
-
-
-/***/ })
-/******/ ]);
+      /***/
+    }
+    /******/
+  ]
+);
